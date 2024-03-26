0dd2285025c63b549d39ebb9cb7521c65ed932e1
wowzik1233 <wowzik1233@gmail.com>
2024-03-27 02:30:12 +0300

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by matvey on 27.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
